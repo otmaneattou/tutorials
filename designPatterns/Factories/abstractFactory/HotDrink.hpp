dc59bc3713d683dae7ad4b3c3b776ff253886661
Otmane ATTOU <attouotmane96@gmail.com>
2021-12-20 21:49:18 +0100

    Implementation of  abstract  factory

---
+++ designPatterns/Factories/abstractFactory/HotDrink.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <memory>
+#include <map>
+#include <string>
+using namespace std;
+
+struct HotDrink
+{
+    virtual ~HotDrink() = default;
+
+    virtual void prepare(int volume) = 0;
+};
+
+struct Tea : HotDrink
+{
+
+    void prepare(int volume) override
+    {
+        cout << "Take tea bag, boil water, pour " << volume << "ml, add some lemon" << endl;
+    }
+};
+
+struct Coffee : HotDrink
+{
+    void prepare(int volume) override
+    {
+        std::cout << "Grind some beans, boil water, pour " << volume << "ml, add cream, enjoy!" << std::endl;
+    }
+};
