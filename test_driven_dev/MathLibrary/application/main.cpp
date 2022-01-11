5b5dab34a7b720c4218f130af2dd1d9627f5ac77
Otmane ATTOU <attouotmane96@gmail.com>
2022-01-11 17:46:38 +0100

    Main of MathLibrary

---
+++ test_driven_dev/MathLibrary/application/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "MyMathLib.hpp"
+
+int main(int argc, const char **argv)
+{
+
+    std::cout << "Sqrt(9) is : " << mySqrt(9.0) << std::endl;
+    std::cout << "/* ****** */" << std::endl;
+    return 0;
+}
